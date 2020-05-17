13afd4e0e591a615d988a8684ddc7d18650f79e8
github username <github email address>
2020-05-17 13:56:50 -0700

    Created a login form inside views that allows a user to log in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,12 @@ class UsersController < ApplicationController
     erb :login #render the login form/page
   end
 
+  post '/login' do
+    #action = login, method = POST.
+    #creating a session. receive login form.
+    binding.pry
+  end
+
+
 
 end
