e4871083d6e6fb0b98fc29a280c27bc36cacf825
github username <github email address>
2020-05-18 11:29:57 -0700

    Added logic in post/cases route to ensure all categories are filled in, and rearranged nav bar in layout to make more flow sense

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -5,6 +5,28 @@ class CasesController < ApplicationController
   end
 
   post '/cases' do
-  end
+    #raise params.inspect
+    #create a new case and save it to the database
+      #only if all symptoms are accounted for
+      #and if a user is logged in
+      if !logged_in?
+        redirect '/'
+      end
 
+      if (params[:cough] != "" && params[:difficulty_breathing] != "" && params[:fever] != "" && params[:chills] != "" && params[:muscle_pain] != "" && params[:sore_throat] != "" && params[:smell_or_taste_loss] != "")
+        @case = Case.create(
+          cough: params[:cough],
+          difficulty_breathing: params[:difficulty_breathing],
+          fever: params[:fever],
+          chills: params[:chills],
+          muscle_pain: params[:muscle_pain],
+          sore_throat: params[:sore_throat],
+          smell_or_taste_loss: params[:smell_or_taste_loss],
+          user_id: current_user.id
+          )
+          redirect "/cases/#{@case.id}"
+      else
+        redirect '/cases/new'
+      end
+  end
 end
