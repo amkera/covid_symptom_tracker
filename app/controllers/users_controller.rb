d000939466c1b614adf7712fddafe32d88a5d627
github username <github email address>
2020-05-17 17:37:55 -0700

    Fixed no-end bug in application controller, added dynamic users/:id route in users controller, created signup and show.erb inside views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,28 +5,31 @@ class UsersController < ApplicationController
   end
 
   post '/login' do
-    #action = login, method = POST. creating a session. receive login form.
-    #params looks like this:
-      #{"email" => "some_user", "password" => "their_password"}
-    #find the user
     @user = User.find_by(email: params[:email])
-    #authenticate
     if @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect 'users/#{@user.id}'
-    else
-
+      redirect "users/#{@user.id}"
     end
-
   end
 
-  get '/signup' do
+  get '/signup' do #render the signup form
+    erb :signup
+    #because signup is toplevel file inside views, don't need to do '/'
   end
+
+  post '/users' do
+    #create a new user and persist them to db if valid input
+    if params[:name] != "" && params[:email] != "" && params[:password] != ""
+      @user = User.create(params)
+      redirect "/users/#{@user.id}"
+      #this is the actual URL, the HTTP request. Rarely render from a post, patch, or delete request.
+    end
+  end 
 
   get '/users/:id' do
-    'testing'
+    erb :"/users/show"
+    #we usually render in get requests
   end
-
 
 
 end
