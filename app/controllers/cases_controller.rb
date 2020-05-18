5864dcec1e059f82e6e65eaee740d96765c19e68
github username <github email address>
2020-05-18 14:10:38 -0700

    Added a delete link to the show.erb and changed strings to integer to allow for data validation when user wants to create a covid case in case controller

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -9,8 +9,9 @@ class CasesController < ApplicationController
       if !logged_in?
         redirect '/'
       end
+      binding.pry
 
-      if (params[:cough].between?(1,10) && params[:difficulty_breathing].between?(1,10) && params[:fever].between?(1,10) && params[:chills].between?(1,10) && params[:muscle_pain].between?(1,10) && params[:sore_throat].between?(1,10) && params[:smell_or_taste_loss].between?(1,10))
+      if (params[:cough].to_i.between?(1,10) && params[:difficulty_breathing].to_i.between?(1,10) && params[:fever].to_i.between?(1,10) && params[:chills].to_i.between?(1,10) && params[:muscle_pain].to_i.between?(1,10) && params[:sore_throat].to_i.between?(1,10) && params[:smell_or_taste_loss].to_i.between?(1,10))
         @case = Case.create(
           cough: params[:cough],
           difficulty_breathing: params[:difficulty_breathing],
@@ -37,6 +38,10 @@ class CasesController < ApplicationController
   get '/cases/:id/edit' do #take user to cases/edit.erb & render edit form
     @case = Case.find(params[:id])
     erb :'/cases/edit'
+  end
+
+  patch '/cases/:id' do
+    "Hello"
   end
 
 
