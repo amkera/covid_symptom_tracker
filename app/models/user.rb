3bc20b22bb0372450840f7408d0f12b07b95e602
github username <github email address>
2020-05-20 15:53:36 -0700

    Added validations in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
-  #authenticate takes a password as a plain string and checks it against bcrypt's hashing algorithm
   has_secure_password
-  #ActiveRecord password validation, ensures it exists
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
   has_many :cases
 end
+
+#authenticate takes a password as a plain string and checks it against bcrypt's hashing algorithm
+#ActiveRecord password validation, ensures it exists
