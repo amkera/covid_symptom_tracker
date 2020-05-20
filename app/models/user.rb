9e1b2d48878eb9ea7c4df07aa061bd4740fead54
github username <github email address>
2020-05-20 15:51:44 -0700

    cleaned up the models files

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,6 @@
 class User < ActiveRecord::Base
-
-  #authenticate takes a password as a plain string and checks it against bcrypt's
-  #hashing algorithm
-
+  #authenticate takes a password as a plain string and checks it against bcrypt's hashing algorithm
   has_secure_password
+  #ActiveRecord password validation, ensures it exists
   has_many :cases
-  
 end
